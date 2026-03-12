53bc004c24479b27fa7e19e1096e4bf694cbe33f
capucho <capucho@gmail.com>
2026-03-12 14:10:06 +0100

    New theme.

--- wp-config.php
+++ wp-config.php
@@ -20,23 +20,10 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpressapi' );
-
-/** Database username */
-define( 'DB_USER', 'denys' );
-
-/** Database password */
-define( 'DB_PASSWORD', 'zaq1@WSX' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
-
+define('DB_NAME', 'wordpressapi'); // имя базы
+define('DB_USER', 'root');         // пользователь MySQL
+define('DB_PASSWORD', '');         // пароль, если был
+define('DB_HOST', 'localhost');
 /**#@+
  * Authentication unique keys and salts.
  *
@@ -85,7 +72,10 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
+define('WP_MEMORY_LIMIT', '256M');
 
 /* Add any custom values between this line and the "stop editing" line. */
 
---
+++ wp-content/themes/capucho/functions.php
@@ -0,0 +1,73 @@
+    <?php
+    function capucho_support() {
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+
+    }
+
+    add_action('after_setup_theme', 'capucho_support');
+
+    function capucho_menus() {
+        $location = array(
+        'primary' => "Desktop Primary left Sidebar",
+        'footer' => "Footer Menu Items"
+        );
+        register_nav_menus($location);
+
+    }
+
+    add_action('init', 'capucho_menus');
+
+    function capucho_register_styles(){
+        $version = wp_get_theme()->get('Version');
+        wp_enqueue_style('capucho-style',get_template_directory_uri() . "/style.css", array('capucho-bootstrap'), $version, 'all');
+        wp_enqueue_style('capucho-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+        wp_enqueue_style('capucho-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
+    }
+
+    add_action('wp_enqueue_scripts', 'capucho_register_styles');
+
+    function capucho_register_scripts() {
+
+    wp_enqueue_script('jquery');
+
+    wp_enqueue_script('capucho-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array('jquery'), '1.16.0', true);
+
+    wp_enqueue_script('capucho-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery'), '4.4.1', true);
+
+    wp_enqueue_script('capucho-main', get_template_directory_uri(). "/assets/js/main.js", array(), '1.0', true);
+
+    }
+
+    add_action('wp_enqueue_scripts', 'capucho_register_scripts');
+
+function capucho_widget_areas() {
+    register_sidebar(
+        array(
+            'name'          => 'Sidebar area', 
+            'id'            => 'sidebar-1',  
+            'description'   => 'Sidebar Widget area',
+            'before_title'  => '',   
+            'after_title'   => '',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name'          => 'Footer area', 
+            'id'            => 'footer-1',  
+            'description'   => 'Footer Widget area',
+            'before_title'  => '',   
+            'after_title'   => '',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+        )
+    );
+}
+
+add_action('widgets_init', 'capucho_widget_areas');
+
+    ?>
